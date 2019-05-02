078630af363087c7207020a8c2cdd0b31ca9e8a8
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-01 21:21:30 -0700

    setting up bandsintown

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.ba14900fd8db4a02a5a3fca0fe13f4e7,
-  secret: process.env.5482b624b1eb4b4d9aeb4314ded785ba
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
 };
--- liri.js
+++ liri.js
@@ -12,3 +12,44 @@ var keys = require("./keys.js");
 const userArguments = process.argv.slice(2);
 
 console.log(userArguments);
+
+if (userArguments[0] === "concert-this") {
+    // Grab the axios package...
+    var axios = require("axios");
+
+    var artist = userArguments[1];
+    console.log(artist);
+
+    axios
+        .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
+}
+if (userArguments[0] === "spotify-this-song") {
+
+}
+if (userArguments[0] === "movie-this") {
+
+}
+if (userArguments[0] === "do-what-it-says") {
+
+}
