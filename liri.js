7a52734b7c58cbad7fa429402eb120732126bc7f
Cefalu Wave <luke.evan.graham@gmail.com>
2019-05-04 20:45:14 -0700

    Bands in Town working, movies working too

--- liri.js
+++ liri.js
@@ -11,11 +11,11 @@ var keys = require("./keys.js");
 // Take in the command line arguments
 const userArguments = process.argv.slice(2);
 
-console.log(userArguments);
+// console.log(userArguments);
 
 const searchTerm = userArguments.slice(1).join(' ');
 
-console.log(searchTerm);
+// console.log(searchTerm);
 
 if (userArguments[0] === "concert-this") {
     // Grab the axios package...
@@ -25,17 +25,24 @@ if (userArguments[0] === "concert-this") {
     console.log(artist);
 
     axios
-        .get("http://www.bandsintown.com/event/?app_id=codingbootcamp&artist=" + artist + "&date=upcoming")
-        .then(function(response) {
+        .get("http://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
+        .then(function (response) {
             // If the axios was successful...
             // Then log the body from the site!
-            console.log(response.data);
+            console.log(response.data[0]);
+
+            console.log('\n\n');
+            console.log('Venue name: ' + response.data[0].venue.name);
+            console.log('Venue location: ' + response.data[0].venue.city + ", " + response.data[0].venue.country);
+            // console.log('Date of Event: ' + data.tracks.items[0].external_urls.spotify);
+            // console.log('Album: ' + data.tracks.items[0].album.name);
+            console.log('\n\n');
 
 
 
 
 
-          })
+        })
         .catch(function (error) {
             if (error.response) {
                 // The request was made and the server responded with a status code
@@ -57,38 +64,72 @@ if (userArguments[0] === "concert-this") {
 }
 if (userArguments[0] === "spotify-this-song") {
     var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify(keys.spotify);
 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-spotify.search({ type: 'track', query: searchTerm }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-   
-//   console.log(data.tracks.items[0].external_urls.spotify); 
+    var spotify = new Spotify(keys.spotify);
 
-  console.log('\n\n');
-  console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
-  console.log('Song name: ' + data.tracks.items[0].name);
-  console.log('Preview: ' + data.tracks.items[0].external_urls.spotify);
-  console.log('Album: ' + data.tracks.items[0].album.name);
-  console.log('\n\n');
-  
-     
-// spotify
-// .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-// .then(function(data) {
-//   console.log(data); 
+    // spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    spotify.search({ type: 'track', query: searchTerm }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
 
+        //   console.log(data.tracks.items[0].external_urls.spotify); 
 
+        console.log('\n\n');
+        console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
+        console.log('Song name: ' + data.tracks.items[0].name);
+        console.log('Preview: ' + data.tracks.items[0].external_urls.spotify);
+        console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('\n\n');
 
-})
-// .catch(function(err) {
-//   console.error('Error occurred: ' + err); 
-// });
+    })
+
 }
 if (userArguments[0] === "movie-this") {
+
+    // var movieInput = userArguments[1];
+    console.log(searchTerm);
+
+    var axios = require("axios");
+
+    axios
+        .get("http://www.omdbapi.com/?apikey=trilogy&t=" + searchTerm)
+        .then(function (response) {
+            // If the axios was successful...
+            // Then log the body from the site!
+            console.log(response.data);
+
+            console.log('\n\n');
+            console.log('Title: ' + response.data.Title);
+            console.log('Year Released: ' + response.data.Year);
+            console.log('IMDB Rating: ' + response.data.imdbRating + "/10");
+            console.log('Rotten Tomatoes Rating: ' + response.data.Ratings[1].Value);
+            console.log('Language: ' + response.data.Language);
+            console.log('Plot: ' + response.data.Plot);
+            console.log('Actors: ' + response.data.Actors);
+            console.log('\n\n');
+
+
+
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+
 
 }
 if (userArguments[0] === "do-what-it-says") {
